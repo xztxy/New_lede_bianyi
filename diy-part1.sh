4c7af7f0ac6248df9aba2aa1a585203ce2704884
xztxy <31698398+xztxy@users.noreply.github.com>
2024-06-18 08:37:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,4 +8,5 @@ sed -i "s/src-git/src-git-full/g" feeds.conf.default
 sed -i "s/src-git-full-full/src-git-full/g" feeds.conf.default
 
 # 添加feeds源
-#echo 'src-git-full helloworld https://github.com/fw876/helloworld.git;main' >> feeds.conf.default
+echo 'src-git-full helloworld https://github.com/fw876/helloworld.git;main' >> feeds.conf.default
+echo "src-git lucky https://github.com/gdy666/luci-app-lucky.git" >> ./feeds.conf.defaultt
