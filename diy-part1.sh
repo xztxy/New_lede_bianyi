5460c4f5600a71c7fb381632c96a6e7a27cbe483
xztxy <31698398+xztxy@users.noreply.github.com>
2025-03-01 15:05:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,18 +1,14 @@
 # 文件名: diy-part1.sh
-# 描述: OpenWrt DIY script part 1 (放在更新feeds之前)
 #!/bin/bash
-
-# 修改feeds源
-# curl -fsSL https://raw.githubusercontent.com/ywt114/diy/main/feeds > feeds.conf.default
-# #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-# sed -i "s/src-git/src-git-full/g" feeds.conf.default
-# sed -i "s/src-git-full-full/src-git-full/g" feeds.conf.default
-
-# 添加feeds源
-# echo 'src-git-full helloworld https://github.com/fw876/helloworld.git;main' >> feeds.conf.default
-# echo 'src-git-full lucky https://github.com/gdy666/luci-app-lucky.git;main' >> feeds.conf.default
-# echo 'src-git op_soft https://github.com/xztxy/op_soft.git;main' >> feeds.conf.default
-# 添加带 post-install 的 Feed
-echo 'src-git peditx https://github.com/peditx/luci-theme-peditx.git;main ^post-install="mkdir -p ${FEEDS_DIR}/peditx/luci-theme-peditx && mv ${FEEDS_DIR}/peditx/* ${FEEDS_DIR}/peditx/luci-theme-peditx/"' >> feeds.conf.default
-# 添加其他 Feed
+# 清理历史残留（关键步骤）
+rm -rf feeds/peditx 2>/dev/null
+# 标准 Feed 声明（不带 post-install）
+echo 'src-git peditx https://github.com/peditx/luci-theme-peditx.git;main' >> feeds.conf.default
 echo 'src-git kwrt-packages https://github.com/kiddin9/kwrt-packages.git;main' >> feeds.conf.default
+# 新增目录修正步骤（在 feeds update 前执行）
+cat << EOF >> feeds.conf.default
+post_install() {
+    mkdir -p feeds/peditx/luci-theme-peditx
+    mv -f feeds/peditx/* feeds/peditx/luci-theme-peditx/ 2>/dev/null || true
+}
+EOF
